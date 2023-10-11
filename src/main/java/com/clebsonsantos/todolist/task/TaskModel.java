8849249114022a6f0c7954f3ceca96087de6f76f
Clebson Santos <clebsonsantosmtt@gmail.com>
2023-10-11 10:58:09 -0300

    feat: add TaskModel

---
+++ src/main/java/com/clebsonsantos/todolist/task/TaskModel.java
@@ -0,0 +1,96 @@
+package com.clebsonsantos.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_task")
+public class TaskModel {
+
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+  private String description;
+
+  @Column(length = 50)
+  private String title;
+  private String priority;
+  private LocalDateTime startAt;
+  private LocalDateTime endAt;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  private UUID idUser;
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getPriority() {
+    return priority;
+  }
+
+  public void setPriority(String priority) {
+    this.priority = priority;
+  }
+
+  public LocalDateTime getStartAt() {
+    return startAt;
+  }
+
+  public void setStartAt(LocalDateTime startAt) {
+    this.startAt = startAt;
+  }
+
+  public LocalDateTime getEndAt() {
+    return endAt;
+  }
+
+  public void setEndAt(LocalDateTime endAt) {
+    this.endAt = endAt;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public UUID getIdUser() {
+    return idUser;
+  }
+
+  public void setIdUser(UUID idUser) {
+    this.idUser = idUser;
+  }
+
+}
